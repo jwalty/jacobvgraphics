42c3be9992764cfcd0e0f817a7199b779eedba5f
JWalty <therealjwalty@gmail.com>
2023-02-22 13:29:30 -0500

    Add alt tags for each image

--- main.js
+++ main.js
@@ -7,14 +7,11 @@ allPortfolioImages.forEach((img) => {
     
     let imageContainer = document.createElement("div");
     let imageOverlay = document.createElement("div");
-    let overlayText = document.createElement("div");
-
     imageContainer.classList.add("imageContainer");
     imageOverlay.classList.add("imageOverlay");
-    
     imageContainer.appendChild(img);
     imageContainer.appendChild(imageOverlay);
-    
-
     portfolioContainer.appendChild(imageContainer);
+
+    
 });
