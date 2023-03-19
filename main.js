88f8b9220b767018fdf227ab151c9ab2354b936b
JWalty <therealjwalty@gmail.com>
2023-03-19 08:44:13 -0400

    Trim lightbox

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 let allPortfolioLinks = document.querySelectorAll("#imagePortfolio > a");
 
 allPortfolioLinks.forEach((link) => {
-
     let image = document.createElement('img');
     image.setAttribute('src',link.getAttribute('href'));
     link.appendChild(image);
-
 });
